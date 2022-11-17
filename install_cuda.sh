3ac3f7c0b3e68bd699f5e59d7ec79af44ecb723f
FindHao <find@findhao.net>
2022-11-17 22:08:23 +0000

    update cuda install

--- install_cuda.sh
+++ install_cuda.sh
@@ -16,7 +16,7 @@ chmod +x cuda_11.6.2_510.47.03_linux.run
 ./cuda_11.6.2_510.47.03_linux.run --toolkit --toolkitpath=${cuda116_path} --silent
 wget -q https://developer.download.nvidia.com/compute/redist/cudnn/v8.3.2/local_installers/11.5/cudnn-linux-x86_64-8.3.2.44_cuda11.5-archive.tar.xz -O cudnn-linux-x86_64-8.3.2.44_cuda11.5-archive.tar.xz
 tar xf cudnn-linux-x86_64-8.3.2.44_cuda11.5-archive.tar.xz
-cuda_path=${cuda116_path} cudnn_path=$(pwd)/cudnn-linux-x86_64-8.3.2.44_cuda11.5-archive ./install_cudnn.sh
+cuda_path=${cuda116_path} cudnn_path=$(pwd)/cudnn-linux-x86_64-8.3.2.44_cuda11.5-archive ../../install_cudnn.sh
 
 wget -q https://developer.download.nvidia.com/compute/cuda/11.7.0/local_installers/cuda_11.7.0_515.43.04_linux.run -P ./.downloads/cu117
 cd ./.downloads/cu117
@@ -24,7 +24,7 @@ chmod +x cuda_11.7.0_515.43.04_linux.run
 ./cuda_11.7.0_515.43.04_linux.run --toolkit --toolkitpath=${cuda117_path} --silent
 wget -q https://ossci-linux.s3.amazonaws.com/cudnn-linux-x86_64-8.5.0.96_cuda11-archive.tar.xz -O cudnn-linux-x86_64-8.5.0.96_cuda11-archive.tar.xz
 tar xf cudnn-linux-x86_64-8.5.0.96_cuda11-archive.tar.xz
-cuda_path=${cuda117_path} cudnn_path=$(pwd)/cudnn-linux-x86_64-8.5.0.96_cuda11-archive ./install_cudnn.sh
+cuda_path=${cuda117_path} cudnn_path=$(pwd)/cudnn-linux-x86_64-8.5.0.96_cuda11-archive ../../install_cudnn.sh
 
 
 
