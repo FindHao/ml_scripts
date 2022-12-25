feec39ec365322977f33e502d195898150da2f1c
FindHao <find@findhao.net>
2022-12-25 17:02:27 -0500

    update comment

--- print_all_task.sh
+++ print_all_task.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# This script prints all models' categories in torchbenchmark/models
 benchmark_path=${benchmark_path:-"/home/yhao24/p/p8/benchmark"}
 all_models=`ls $benchmark_path/torchbenchmark/models`
 cd $benchmark_path/
