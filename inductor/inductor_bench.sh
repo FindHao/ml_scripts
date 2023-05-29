aa40bc201b854e1b9f868ec8d20c0e8a8aba31bf
FindHao <find@findhao.net>
2023-05-29 16:42:31 -0700

    update

--- inductor/inductor_bench.sh
+++ inductor/inductor_bench.sh
@@ -17,17 +17,30 @@ source ${SHELL_FOLDER}/../run_base.sh
 conda activate $env1
 source $cuda_env1
 
+cold_start_latency=${cold_start_latency:-0}
+if [ $cold_start_latency -eq 1 ]; then
+    cold_start_latency_placeholder="--cold-start-latency"
+else
+    cold_start_latency_placeholder=""
+fi
+enable_debug=${enable_debug:-0}
+if [ $enable_debug -eq 1 ]; then
+    debug_placeholder="env TORCH_COMPILE_DEBUG=1"
+else
+    debug_placeholder=""
+fi
+
+
 # get date
 date_suffix=$(date +%Y%m%d_%H%M%S)
 cd $pytorch_path
 
 if [ "$mode" == "train" ]; then
     mode="--training"
-fi
-if [ "$mode" == "eval" ]; then
+elif [ "$mode" == "eval" ]; then
     mode="--inference"
 fi
 
-python benchmarks/dynamo/huggingface.py --performance --cold-start-latency $mode  --amp --backend inductor --disable-cudagraphs --device cuda >>$output  2>&1
+${debug_placeholder} python benchmarks/dynamo/huggingface.py --performance ${cold_start_latency_placeholder} $mode  --amp --backend inductor --disable-cudagraphs --device cuda >>$output  2>&1
 
 notify 
