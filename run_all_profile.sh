4136f6fce29647383bbfcbe2ca828c59e1ef4b55
FindHao <find@findhao.net>
2023-05-31 14:18:08 -0700

    update profile

--- run_all_profile.sh
+++ run_all_profile.sh
@@ -1,72 +1,43 @@
 #!/bin/bash
 
-SHELL_FOLDER=$(cd "$(dirname "$0")";pwd)
+SHELL_FOLDER=$(
+    cd "$(dirname "$0")"
+    pwd
+)
 source ${SHELL_FOLDER}/run_base.sh
 
-profile_suffix=logs_profile_${mode}
-if [ $tb_env1 ] && [ $tb_env1 != "pt_sep14" ]; then
-    profile_suffix=logs_profile_${mode}_$env1_$(date +'%Y%m%d%H%M')
-fi
+profile_suffix=logs_profile_${mode}_$env1_$(date +'%Y%m%d%H%M')
 
 enable_inductor=${enable_inductor:-0}
 
 cd $tb_path
-func(){
-    for (( i = 1 ; i <= $max_iter; i++ ))
-    do
+func() {
+    for ((i = 1; i <= $max_iter; i++)); do
         # python run.py -d cuda -m jit -t train $model --precision fp32 --torchdynamo nvfuser  >> $output 2>&1
-        python run.py -d cuda -t $mode --profile --profile-detailed --profile-devices cpu,cuda --profile-folder ${work_path}/${profile_suffix}/$model  $model   >> $output 2>&1
+        python run.py -d cuda -t $mode ${jit_placeholder} ${amp_placeholder} --profile --profile-detailed --profile-devices cpu,cuda --profile-folder ${work_path}/${profile_suffix}/$model $model ${inductor_placeholder} >>$output 2>&1
         if [ $? -ne 0 ]; then
             break
         fi
     done
 }
-
-func_jit(){
-    for (( i = 1 ; i <= $max_iter; i++ ))
-    do
-        python run.py -d cuda -m jit -t $mode --profile --profile-detailed --profile-devices cpu,cuda --profile-folder ${work_path}/${profile_suffix}/$model  $model  --precision fp32  >> $output 2>&1
-        if [ $? -ne 0 ]; then
-            break
-        fi
-    done
-}
-
-func_inductor(){
-    for (( i = 1 ; i <= $max_iter; i++ ))
-    do
-        python run.py -d cuda -t $mode --profile --profile-detailed --profile-devices cpu,cuda --profile-folder ${work_path}/${profile_suffix}/$model  $model --torchdynamo inductor  >> $output 2>&1
-        if [ $? -ne 0 ]; then
-            break
-        fi
-    done
-}
-
 
 conda activate $env1
 
-
-run_func=func
 if [ $enable_jit -eq 1 ]; then
     profile_suffix=${profile_suffix}_jit
-    run_func=func_jit
 fi
 
 if [ $enable_inductor -eq 1 ]; then
     profile_suffix=${profile_suffix}_inductor
-    run_func=func_inductor
 fi
 
-
-echo `date` >> $output
-for model in phlippe_densenet resnext50_32x4d hf_Reformer timm_vovnet
-# for model in $all_models;
-do 
+echo $(date) >>$output
+# for model in phlippe_densenet resnext50_32x4d hf_Reformer timm_vovnet
+for model in $all_models; do
     echo "@Yueming Hao origin $model" >>$output
-    ${run_func}
+    func
 done
 
-
-echo `date` >> $output
+echo $(date) >>$output
 
 notify
--- run_base.sh
+++ run_base.sh
@@ -3,17 +3,14 @@
 
 var_date=$(date +'%Y%m%d%H%M')
 
-
 # ====config begin======
 # Required configs
 # mode: train or eval
 mode=${mode:-train}
-# the main work path
 work_path=${work_path:-/home/yhao/d}
 conda_dir=${conda_dir:-/home/yhao/d/conda}
 # env1 is the default environment
 env1=${env1:-pt_jan02}
-
 
 if [ ! -d $work_path ]; then
     echo "work_path not exist"
@@ -30,17 +27,36 @@ output=${log_path}/${prefix_filename}_${mode}_${var_date}.log
 
 # env2 is used to speedup comparion scripts like run_all_speedup*
 env2=${env2:-pt_jan02_all}
-enable_jit=${enable_jit:-0}
 cuda_env1=${cuda_env1:-/home/yhao/setenvs/set11.6-cudnn8.3.3.sh}
 # cuda_env2 is used to speedup comparion scripts like run_all_speedup_cuda
 cuda_env2=${cuda_env2:-/home/yhao/setenvs/set11.6-cudnn8.5.0.sh}
 max_iter=${max_iter:-10}
-metrics_gpu_backend=${metrics_gpu_backend:-default}
 # pytorch path
 pytorch_path=${pytorch_path:-$work_path/pytorch}
+
+# run.py arguments
+metrics_gpu_backend=${metrics_gpu_backend:-default}
+enable_jit=${enable_jit:-0}
+if [ $enable_jit -eq 1 ]; then
+    jit_placeholder="-m jit"
+else
+    jit_placeholder=""
+fi
+enable_amp=${enable_amp:-1}
+if [ $enable_amp -eq 1 ]; then
+    amp_placeholder="--amp"
+else
+    amp_placeholder=""
+fi
+enable_inductor=${enable_inductor:-0}
+if [ $enable_inductor -eq 1 ]; then
+    inductor_placeholder="--torchdynamo inductor"
+else
+    inductor_placeholder=""
+fi
+
+
 # ====config end======
-
-
 
 echo $output
 source ${conda_dir}/bin/activate
@@ -53,45 +69,40 @@ if [ ! -d ${work_path}/logs ]; then
     mkdir ${work_path}/logs
 fi
 
-echo "" > $output
-echo `date` >> $output
-echo "torchexpert: $torchexpert" >> $output
-echo "work_path: $work_path" >> $output
-echo "tb_path: $tb_path" >> $output
-echo "output_csv_file: $output" >> $output
-echo "mode: $mode" >> $output
-echo "conda_dir: $conda_dir" >> $output
-echo "conda envs:" >> $output
-echo "env1" $env1 >> $output
-echo "env2" $env2 >> $output
+echo "" >$output
+echo $(date) >>$output
+echo "torchexpert: $torchexpert" >>$output
+echo "work_path: $work_path" >>$output
+echo "tb_path: $tb_path" >>$output
+echo "output_csv_file: $output" >>$output
+echo "mode: $mode" >>$output
+echo "conda_dir: $conda_dir" >>$output
+echo "conda envs:" >>$output
+echo "env1" $env1 >>$output
+echo "env2" $env2 >>$output
 if [ $enable_jit -eq 1 ]; then
-    echo "enable_jit: True" >> $output
+    echo "enable_jit: True" >>$output
 fi
-echo "metrics_gpu_backend: $metrics_gpu_backend" >> $output
-
-
+echo "metrics_gpu_backend: $metrics_gpu_backend" >>$output
 
 # use pushover to notify the end of the script, need extra environment variables PUSHOVER_API PUSHOVER_USER_KEY
-notify()
-{
-    hostname=`cat /proc/sys/kernel/hostname`
+notify() {
+    hostname=$(cat /proc/sys/kernel/hostname)
     curl -s \
-    --form-string "token=${PUSHOVER_API}" \
-    --form-string "user=${PUSHOVER_USER_KEY}" \
-    --form-string "message=${prefix_filename} on ${hostname} done! Output file is ${output}. " \
-    https://api.pushover.net/1/messages.json
+        --form-string "token=${PUSHOVER_API}" \
+        --form-string "user=${PUSHOVER_USER_KEY}" \
+        --form-string "message=${prefix_filename} on ${hostname} done! Output file is ${output}. " \
+        https://api.pushover.net/1/messages.json
 }
 
-
 # list all folder names under torchbenchmark/models
-all_models=`ls -d ${tb_path}/torchbenchmark/models/*/ | xargs -n 1 basename`
-all_models=`echo $all_models | tr '\n' ' '`
-
+all_models=$(ls -d ${tb_path}/torchbenchmark/models/*/ | xargs -n 1 basename)
+all_models=$(echo $all_models | tr '\n' ' ')
 
 # all_models="detectron2_fasterrcnn_r_101_dc5 drq hf_GPT2_large mobilenet_v3_large resnet152 timm_nfnet BERT_pytorch detectron2_fasterrcnn_r_101_fpn fambench_xlmr hf_Longformer moco resnet18 timm_regnet Background_Matting detectron2_fasterrcnn_r_50_c4 fastNLP_Bert hf_Reformer nvidia_deeprecommender resnet50 timm_resnest DALLE2_pytorch detectron2_fasterrcnn_r_50_dc5 functorch_dp_cifar10 hf_T5 opacus_cifar10 resnet50_quantized_qat timm_vision_transformer LearningToPaint detectron2_fasterrcnn_r_50_fpn functorch_maml_omniglot hf_T5_base phlippe_resnet resnext50_32x4d timm_vision_transformer_large Super_SloMo detectron2_fcos_r_50_fpn hf_Albert hf_T5_large pyhpc_equation_of_state shufflenet_v2_x1_0 timm_vovnet alexnet detectron2_maskrcnn hf_Bart lennard_jones pyhpc_isoneutral_mixing soft_actor_critic tts_angular attention_is_all_you_need_pytorch detectron2_maskrcnn_r_101_c4 hf_Bert maml pyhpc_turbulent_kinetic_energy speech_transformer vgg16 dcgan detectron2_maskrcnn_r_101_fpn hf_Bert_large maml_omniglot pytorch_CycleGAN_and_pix2pix squeezenet1_1 vision_maskrcnn demucs detectron2_maskrcnn_r_50_c4 hf_BigBird mnasnet1_0 pytorch_stargan tacotron2 yolov3 densenet121 detectron2_maskrcnn_r_50_fpn hf_DistilBert mobilenet_v2 pytorch_struct timm_efficientdet detectron2_fasterrcnn_r_101_c4 dlrm hf_GPT2 mobilenet_v2_quantized_qat pytorch_unet timm_efficientnet"
 
 check_conda_env_exist() {
-    if [[ $( conda env list | grep -c "${conda_env}" ) -eq 0 ]]; then
+    if [[ $(conda env list | grep -c "${conda_env}") -eq 0 ]]; then
         echo "Conda environment ${conda_env} does not exist."
         exit 1
     fi
