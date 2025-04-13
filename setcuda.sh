b6797f9c17c704fa0356d7b344dfa0a5f6af93a1
FindHao <yhao@meta.com>
2025-04-12 22:19:10 -0700

    clean up code

--- setcuda.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-export CUDA_PATH=/scratch/opt/cuda-12.1
-export CUDA_HOME=$CUDA_PATH
-export PATH=$CUDA_PATH/bin:$PATH
-export LD_LIBRARY_PATH=$CUDA_PATH/lib64:$CUDA_PATH/compute-sanitizer:$LD_LIBRARY_PATH 
-export CUDA_TOOLKIT_ROOT_DIR=$CUDA_PATH
-source /scratch/setenvs/setncu_nsys.sh
