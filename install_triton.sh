85a3b2be5eb3736fbaed06fad156aaf98f4e959a
Yueming Hao <yhao@meta.com>
2025-06-09 17:34:31 +0000

    add triton installation

---
+++ install_triton.sh
@@ -0,0 +1,18 @@
+
+# This file is not a runnable script but a set of instructions to install Triton.
+
+conda create -n pta python=3.11
+conda activate pta
+conda install  libstdcxx-ng lld zstd zlib libgcc-ng clang=20 clangxx=20 ninja ccache cmake -y -c conda-forge
+export CMAKE_PREFIX_PATH="$CONDA_PREFIX"
+export LIBRARY_PATH=$CONDA_PREFIX/lib:$LIBRARY_PATH
+export LD_LIBRARY_PATH=$CONDA_PREFIX/lib:$LD_LIBRARY_PATH
+export CXXFLAGS="-I$CONDA_PREFIX/include"
+export LDFLAGS="-L$CONDA_PREFIX/lib"
+
+# if reports error `Host compiler does not support '-fuse-ld=lld'.`
+# set -DLLVM_ENABLE_LLD=OFF in `scripts/build-llvm-project.sh`
+
+rm -rf .llvm-project build
+make dev-install-llvm
+
