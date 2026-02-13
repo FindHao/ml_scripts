2ffea4d7e2a01c56291b7e98b671929aab2dd623
Yueming Hao <yhao@meta.com>
2026-02-13 11:40:35 -0800

    update clang version

--- install_triton.sh
+++ install_triton.sh
@@ -2,9 +2,9 @@
 # Sometimes, pip install triton does not work due to various issues. This script will let triton installation use
 # the conda environment to install the required dependencies.
 
-conda create -n pta python=3.11
+conda create -n pta python=3.13
 conda activate pta
-conda install libstdcxx-ng lld zstd zlib libgcc-ng clang=20 clangxx=20 ninja ccache cmake -y -c conda-forge
+conda install libstdcxx-ng lld zstd zlib libgcc-ng clang=21 clangxx=21 ninja ccache cmake -y -c conda-forge
 export CMAKE_PREFIX_PATH="$CONDA_PREFIX"
 export LIBRARY_PATH=$CONDA_PREFIX/lib:$LIBRARY_PATH
 export LD_LIBRARY_PATH=$CONDA_PREFIX/lib:$LD_LIBRARY_PATH
